08129f642072a5dfcb4f8e24762ad790d3dc46f3
“Dia6 <dianafaran6@gmail.com>
2021-03-29 21:17:21 +0300

    Verificare Commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,19 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Welcome to code, Diana!");
 
+        System.out.println("Hello");
+        System.out.println("Diana");
+
+       int x = 6;
+       int y = 3;
+        System.out.println(x+y);
+
+        float result = 9f/2;
+        System.out.println(result);
+
+        System.out.println(-5 +8 *6);
+
+        double result1 = -5 +8 *6;
+        System.out.println("Rezultatul este " +result1);
     }
 }
