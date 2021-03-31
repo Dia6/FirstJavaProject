e563c1b9aed40bdcbdb1f3d4cde90e0d259a1fab
“Dia6 <dianafaran6@gmail.com>
2021-03-31 23:04:02 +0300

    Tema

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello World");
         System.out.println("Welcome to code, Diana!");
 
         System.out.println("Hello");
         System.out.println("Diana");
 
-       int x = 6;
-       int y = 3;
-        System.out.println(x+y);
+        int x = 6;
+        int y = 3;
+        System.out.println(x + y);
 
-        float result = 9f/2;
+        float result = 9f / 2;
         System.out.println(result);
 
-        System.out.println(-5 +8 *6);
+        System.out.println(-5 + 8 * 6);
 
-        double result1 = -5 +8 *6;
-        System.out.println("Rezultatul este " +result1);
+        int resulta = -5 + 8 * 6;
+        System.out.println("Rezultatul este " + resulta);
+
+        double resultb = (55 + 9) % 9;
+        System.out.println("Rezultatul este " + resultb);
+
+        int resultc = 20 + ((-3 * 5) / 8);
+        System.out.println("Rezultatul este " + resultc);
+
+        double resultd = 5 + 15d / 3 * 2 - 8 % 3;
+        System.out.println("Rezultatul este " + resultd);
+
     }
 }
