b9ade4eba4f9459106faf2500e83c2692a2883dc
“Dia6 <dianafaran6@gmail.com>
2021-04-06 22:21:35 +0300

    Tema Lab 3 Encapsulare

--- src/Lab2/AnimalRescue/Food.java
+++ src/Lab2/AnimalRescue/Food.java
@@ -1,9 +1,46 @@
 package Lab2.AnimalRescue;
 
 public class Food {
-    String name = "Petkult";
-    String type = "grains";
-    float priceInLei = 25;
-    float quantityInKg = 0.25f;
-    float availableQtyInKg = 175f;
+    private String name = "Petkult";
+    private String type = "grains";
+    private float priceInLei = 25;
+    private float quantityInKg = 0.25f;
+    private float availableQtyInKg = 175f;
+
+    public float getAvailableQtyInKg() {
+        return availableQtyInKg;
+    }
+    public void setAvailableQtyInKg(float availableQtyInKg) {
+        this.availableQtyInKg = availableQtyInKg;
+    }
+
+    public float getQuantityInKg() {
+        return quantityInKg;
+    }
+    public void setQuantityInKg(float quantityInKg) {
+        this.quantityInKg = quantityInKg;
+    }
+
+    public float getPriceInLei() {
+        return priceInLei;
+    }
+    public void setPriceInLei(float priceInLei) {
+        this.priceInLei = priceInLei;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
